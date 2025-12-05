4c51277173767bc30d5552d5f73c071c5eb0d76e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-12-05 06:06:55 +0000

    Deploying to gh-pages from @ libscran/scran_variances@c1ded270b3ab04d764ccd90585ec373a41d5f574 🚀

--- docs/search/classes_b.js
+++ docs/search/classes_b.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['singlequantile_0',['SingleQuantile',['https://libscran.github.io/scran_blocks/classscran__blocks_1_1SingleQuantile.html',1,'scran_blocks']]],
-  ['somenumericarray_1',['SomeNumericArray',['https://tatami-inc.github.io/tatami/classtatami_1_1SomeNumericArray.html',1,'tatami']]],
-  ['sortby_2',['SortBy',['https://ltla.github.io/CppWeightedLowess/classWeightedLowess_1_1SortBy.html',1,'WeightedLowess']]],
-  ['sparserange_3',['SparseRange',['https://tatami-inc.github.io/tatami/structtatami_1_1SparseRange.html',1,'tatami']]]
+  ['singlequantilevariable_1',['SingleQuantileVariable',['https://libscran.github.io/scran_blocks/classscran__blocks_1_1SingleQuantileVariable.html',1,'scran_blocks']]],
+  ['somenumericarray_2',['SomeNumericArray',['https://tatami-inc.github.io/tatami/classtatami_1_1SomeNumericArray.html',1,'tatami']]],
+  ['sortby_3',['SortBy',['https://ltla.github.io/CppWeightedLowess/classWeightedLowess_1_1SortBy.html',1,'WeightedLowess']]],
+  ['sparserange_4',['SparseRange',['https://tatami-inc.github.io/tatami/structtatami_1_1SparseRange.html',1,'tatami']]]
 ];
